3bc8b402052139efb58e76d11ea6932725f3b563
Adir Kuhn <adirkuhn@gmail.com>
2015-10-20 15:17:09 -0200

    Add seeds and user-plan association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016172245) do
+ActiveRecord::Schema.define(version: 20151019231444) do
 
   create_table "moots", force: :cascade do |t|
     t.string   "title"
@@ -45,9 +45,11 @@ ActiveRecord::Schema.define(version: 20151016172245) do
     t.boolean  "is_admin"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["plan_id"], name: "index_users_on_plan_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+plans = Plan.create([
+  { title: 'Assembleia 5', description: 'Plano simples permite criar 5 assembléias por ano', max_moot_per_year: 5 },
+  { title: 'Assembleia 10', description: 'Plano medio permite criar 10 assembléias por ano', max_moot_per_year: 10 },
+  { title: 'Assembleia 20', description: 'Plano pro permite criar 10 assembléias por ano', max_moot_per_year: 20 }
+])
+
