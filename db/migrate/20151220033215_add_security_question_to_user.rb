52ec4504ae14938c9aa59485a18cc868aba12cad
Adir Kuhn <adirkuhn@gmail.com>
2016-01-04 00:15:50 +0800

    user question

--- db/migrate/20151220033215_add_security_question_to_user.rb
+++ db/migrate/20151220033215_add_security_question_to_user.rb
@@ -1,8 +1,8 @@
 class AddSecurityQuestionToUser < ActiveRecord::Migration
   def change
-    add_column :users, :securityQuestion1_id, :integer, references: :users
-    add_column :users, :securityQuestion2_id, :integer, references: :users
-    add_column :users, :securityQuestion3_id, :integer, references: :users
+    add_column :users, :securityQuestion1_id, :integer, references: :securityQuestion
+    add_column :users, :securityQuestion2_id, :integer, references: :securityQuestion
+    add_column :users, :securityQuestion3_id, :integer, references: :securityQuestion
     add_column :users, :securityAnswer1, :string
     add_column :users, :securityAnswer2, :string
     add_column :users, :securityAnswer3, :string
