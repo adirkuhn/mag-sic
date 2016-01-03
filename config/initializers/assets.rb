14df0c88becf481e3faf06f255102d608b7daa3b
Adir Kuhn <adirkuhn@gmail.com>
2016-01-04 00:53:33 +0800

    heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [
+    "application.css",
     "home.js", "home.css",
     "devise/sessions.js", "devise/sessions.css",
     "users/registrations.js", "users/registrations.css",
